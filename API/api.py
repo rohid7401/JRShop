9ebb0a65c1e31a1c150d5053589cc34459cb9150
Jocxan S <jocxansandi7@estudiantec.cr>
2023-05-16 15:00:31 -0600

    update

--- API/api.py
+++ API/api.py
@@ -11,29 +11,29 @@ db = pymysql.connect(
     database='database_name'
 )
 
-# Función para ejecutar consultas SQL
-def execute_query(query, params=None):
+# Función para ejecutar los procedimientos almacenados en la base de datos
+def execute_procedure(proc_name, args=None):
     cursor = db.cursor()
-    if params:
-        cursor.execute(query, params)
+    if args:
+        cursor.callproc(proc_name, args)
     else:
-        cursor.execute(query)
+        cursor.callproc(proc_name)
     db.commit()
-    return cursor
+    result = cursor.fetchall()
+    return result
 
 # Obtener todos los usuarios
-@app.route('/users', methods=['GET'])
+@app.route('/get_all_users', methods=['GET'])
 def get_all_users():
-    query = "SELECT * FROM users"
-    result = execute_query(query)
+    result = execute_procedure('get_all_users')
     users = [{'id': row[0], 'name': row[1], 'email': row[2], 'shipping_address': row[3]} for row in result]
     return jsonify(users)
 
 # Obtener un usuario por su ID
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user_by_id(user_id):
-    query = "SELECT * FROM users WHERE id = %s"
-    result = execute_query(query, (user_id,))
+@app.route('/get_user_by_id', methods=['POST'])
+def get_user_by_id():
+    user_id = request.json['id']
+    result = execute_procedure('get_user_by_id', (user_id,))
     user = result.fetchone()
     if user:
         user_data = {'id': user[0], 'name': user[1], 'email': user[2], 'shipping_address': user[3]}
@@ -42,34 +42,33 @@ def get_user_by_id(user_id):
         return jsonify({'message': 'User not found'}), 404
 
 # Insertar un nuevo usuario
-@app.route('/users', methods=['POST'])
+@app.route('/insert_user', methods=['POST'])
 def insert_user():
     data = request.get_json()
     name = data['name']
     email = data['email']
     password = data['password']
     shipping_address = data['shipping_address']
-    query = "INSERT INTO users (name, email, password, shipping_address) VALUES (%s, %s, %s, %s)"
-    execute_query(query, (name, email, password, shipping_address))
+    execute_procedure('insert_user', (name, email, password, shipping_address))
     return jsonify({'message': 'User created successfully'}), 201
 
 # Actualizar un usuario existente
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
+@app.route('/update_user', methods=['POST'])
+def update_user():
     data = request.get_json()
+    user_id = data['id']
     name = data['name']
     email = data['email']
     password = data['password']
     shipping_address = data['shipping_address']
-    query = "UPDATE users SET name = %s, email = %s, password = %s, shipping_address = %s WHERE id = %s"
-    execute_query(query, (name, email, password, shipping_address, user_id))
+    execute_procedure('update_user', (user_id, name, email, password, shipping_address))
     return jsonify({'message': 'User updated successfully'})
 
 # Eliminar un usuario
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    query = "DELETE FROM users WHERE id = %s"
-    execute_query(query, (user_id,))
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    user_id = request.json['id']
+    execute_procedure('delete_user', (user_id,))
     return jsonify({'message': 'User deleted successfully'})
 
 if __name__ == '__main__':
